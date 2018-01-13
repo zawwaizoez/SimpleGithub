15cdb67c92ae49754efad26d62e9db097534637f
zawwaizoe <rohinjar456+1@gmail.com>
2018-01-13 10:49:38 -0800

    buttons changed

--- app/src/main/java/com/example/kryptonite/simplegithub/MainActivity.java
+++ app/src/main/java/com/example/kryptonite/simplegithub/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.kryptonite.simplegithub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn1;
+    private Button btn1,btn2;
     private TextView tv1;
+    private EditText et1;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
