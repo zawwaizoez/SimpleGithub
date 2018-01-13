ce2148d2a86a549486e6d0f4b8b4da160a0f84e4
zawwaizoe <rohinjar456+1@gmail.com>
2018-01-13 10:44:48 -0800

    Two Buttons added

--- app/src/main/java/com/example/kryptonite/simplegithub/MainActivity.java
+++ app/src/main/java/com/example/kryptonite/simplegithub/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.kryptonite.simplegithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private Button btn1;
+    private TextView tv1;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
